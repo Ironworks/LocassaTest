c3e483253a1546b8e7737cd292ed331cde2e7f19
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2015-03-07 11:10:26 +0000

    Initial implementation of layout code.

---
+++ LocassaTest/UICollectionView/BoxLayout.h
@@ -0,0 +1,13 @@
+//
+//  BoxLayout.h
+//  LocassaTest
+//
+//  Created by Trevor Doodes on 07/03/2015.
+//  Copyright (c) 2015 Ironworks Media Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoxLayout : UICollectionViewLayout
+
+@end
---
+++ LocassaTest/UICollectionView/BoxLayout.m
@@ -0,0 +1,158 @@
+//
+//  BoxLayout.m
+//  LocassaTest
+//
+//  Created by Trevor Doodes on 07/03/2015.
+//  Copyright (c) 2015 Ironworks Media Limited. All rights reserved.
+//
+
+#import "BoxLayout.h"
+
+static NSString * const BoxCellKind = @"BoxCell";
+
+@interface BoxLayout ()
+
+//CollectionView item size
+@property (nonatomic) CGSize itemSize;
+
+//Horizontal spacing between CollectionView items
+@property (nonatomic) CGFloat interItemSpacingX;
+
+//Vertical spacing between CollectionView items
+@property (nonatomic) CGFloat interItemSpacingY;
+
+//NSDictionary to cache the layout info
+@property (nonatomic,strong) NSDictionary *layoutInfo;
+
+@end
+
+@implementation BoxLayout
+
+//Override init methods to setup initial values
+-(instancetype)init {
+    
+    if (self = [super init]) {
+        [self setup];
+    }
+    
+    return self;
+}
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder {
+    
+    if (self = [super initWithCoder:aDecoder]) {
+        [self setup];
+    }
+    
+    return self;
+}
+
+//Setup initial values
+-(void)setup {
+    
+    self.itemSize = CGSizeMake(100.0, 100.0);
+    self.interItemSpacingX = 10.0;
+    self.interItemSpacingY = 10.0;
+    
+}
+
+#pragma mark -
+#pragma mark - Layout
+-(void)prepareLayout {
+    
+    NSMutableDictionary *newLayoutInfo = [NSMutableDictionary dictionary];
+    NSMutableDictionary *cellLayoutInfo = [NSMutableDictionary dictionary];
+    
+    NSInteger sectionCount = [self.collectionView numberOfSections];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:0];
+    
+    for (NSInteger section = 0; section < sectionCount; section++) {
+        
+        NSInteger itemCount = [self.collectionView numberOfItemsInSection:section];
+        
+        for (NSInteger item = 0; item < itemCount; item++) {
+            
+            indexPath = [NSIndexPath indexPathForItem:item inSection:section];
+            
+            UICollectionViewLayoutAttributes *itemAttributes =
+            [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+            itemAttributes.frame = [self frameForBoxAtIndexPath:indexPath];
+            
+            cellLayoutInfo[indexPath] = itemAttributes;
+        }
+    }
+    
+    newLayoutInfo[BoxCellKind] = cellLayoutInfo;
+    
+    self.layoutInfo = newLayoutInfo;
+
+}
+
+- (CGRect)frameForBoxAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    //Get max number of cells per row.
+    NSInteger numberOfColumns = self.collectionView.bounds.size.width / (self.itemSize.width + self.interItemSpacingX);
+    
+    NSLog(@"Collection View width: %f", self.collectionView.bounds.size.width);
+    
+    NSInteger row = indexPath.row / numberOfColumns;
+    NSInteger column;
+    if (row == 0) {
+        column = indexPath.row;
+    } else {
+        column = indexPath.row - (numberOfColumns * row);
+    }
+    
+    
+    CGFloat originX = floorf((self.itemSize.width + self.interItemSpacingX) * column);
+    CGFloat originY = floor ((self.itemSize.height + self.interItemSpacingY) * row);
+    
+    return CGRectMake(originX, originY, self.itemSize.width, self.itemSize.height);
+}
+
+-(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    
+    NSMutableArray *allAttributes = [NSMutableArray arrayWithCapacity:self.layoutInfo.count];
+    
+    [self.layoutInfo enumerateKeysAndObjectsUsingBlock:^(NSString *elementIdentifier,
+                                                         NSDictionary *elementInfo,
+                                                         BOOL *stop) {
+        [elementInfo enumerateKeysAndObjectsUsingBlock:^(NSIndexPath *indexPath,
+                                                         UICollectionViewLayoutAttributes *attributes,
+                                                         BOOL *stop) {
+            if (CGRectIntersectsRect(rect, attributes.frame)) {
+                [allAttributes addObject:attributes];
+            }
+        }];
+    }];
+    
+    return allAttributes;
+    
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return self.layoutInfo[BoxCellKind][indexPath];
+}
+
+-(CGSize)collectionViewContentSize {
+    
+    NSInteger numberOfColumns = self.collectionView.bounds.size.width / (self.itemSize.width + self.interItemSpacingX);
+    NSInteger rowCount = [self.collectionView numberOfItemsInSection:0] / numberOfColumns;
+    //Make sure you count another row if one is partially filled
+    if (rowCount % numberOfColumns != 0) {
+        rowCount++;
+    }
+    CGFloat height = rowCount * (self.itemSize.height + self.interItemSpacingY);
+    
+    return CGSizeMake(self.collectionView.bounds.size.width, height);
+}
+
+-(BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    
+    return YES;
+    
+}
+
+
+@end
